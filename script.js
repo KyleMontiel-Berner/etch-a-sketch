2ac17a79014f872a1b7979f81fa78ecd253b381b
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2025-11-06 22:42:10 -0600

    feat: add new game functionality
    added the ability to render a new grid
    
    added event listener to retrieve grid size from the user

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let container = document.querySelector("#container");
 let button = document.createElement("button");
 
+container.appendChild(button);
+button.textContent = "Click Me!"
+
+for (let i = 0; i < 252; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("gridBox");
+    container.appendChild(newDiv);
+};
 
 button.addEventListener('click', ()=> {
     let userInput = prompt("How many squares per side?");
-    let num = userInput.value;
-    return num
+    let squaresPerSide = Number(userInput);
+    renderNewGrid(squaresPerSide);
 });
 
+function renderNewGrid(num) {
+    const removedDivs = document.querySelectorAll(".gridBox");
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("gridBox");
-    container.appendChild(div);
+    removedDivs.forEach(div => {
+        div.remove();
+    });
+
+    for (let i = num; i < (num * num); i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("gridBox");
+        container.appendChild(newDiv);
+    }
 }
+
+
